4df39f7394700dd6f546739da912895fffbf69c2
Sławomir Aleksak <34636@studentwsei.pl>
2021-12-28 08:19:41 +0100

    utworzone wątki i wątek kontynujący

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using JsonConverter = System.Text.Json.Serialization.JsonConverter;
 
@@ -16,8 +17,7 @@ namespace Lab4_sa
         {
             string choosenCurrencyCode = null;
             string urlUsd = "http://api.nbp.pl/api/exchangerates/rates/a/usd/last/30/?format=json";
-            string urlCurrency = "http://api.nbp.pl/api/exchangerates/rates/a/" + choosenCurrencyCode +
-                                 "/last/30/?format=json";
+
             string urlGold = "http://api.nbp.pl/api/cenyzlota/last/30/?format=json";
             string urlAviliableCurrency = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
 
@@ -28,6 +28,19 @@ namespace Lab4_sa
             var currencyRateChoseCurrency = JsonConvert.DeserializeObject<ChooseRoot[]>(resultChoseCurrency);
             Console.WriteLine(currencyRateChoseCurrency);
 
+
+            Task<object> task1 = new Task<object>(
+                () => getCurrencyRate(urlUsd));
+            task1.Start();
+
+            Task<object> task2 = new Task<object>(
+                () => getGoldRate(urlGold));
+            task2.Start();
+            /*            Task<object> task2 = Task.Factory.StartNew<object>(
+                            () => getGoldRate(urlGold));*/
+
+
+
             for (int i = 0; i < currencyRateChoseCurrency[0].rates.Count; i++)
             {
                 Console.WriteLine( i + " " + currencyRateChoseCurrency[0].rates[i].code + " " + currencyRateChoseCurrency[0].rates[i].currency);
@@ -35,107 +48,131 @@ namespace Lab4_sa
             Console.WriteLine("Wybierz nr waluty, którą chcesz wykoszystać do przeliczeń na złoto :");
             int choosenCurrencyId = Convert.ToInt32(Console.ReadLine());
             choosenCurrencyCode = currencyRateChoseCurrency[0].rates[choosenCurrencyId].code;
+            string urlCurrency = "http://api.nbp.pl/api/exchangerates/rates/a/" + choosenCurrencyCode +
+                                 "/last/30/?format=json";
 
 
 
+          
 
 
 
+            object getCurrencyRate(string urlCurrency)
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.GetAsync(urlCurrency).Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                var currencyRate = JsonConvert.DeserializeObject<CurrencyRoot>(result);
+                currencyRate.GetType();
+                return currencyRate;
+            }
+
+            object getGoldRate(string urlGold)
+            {
+                HttpClient clientGold = new HttpClient();
+                HttpResponseMessage responseGold = clientGold.GetAsync(urlGold).Result;
+                string resultGold = responseGold.Content.ReadAsStringAsync().Result;
+                var goldRate = JsonConvert.DeserializeObject<GoldRate[]>(resultGold);
+                goldRate.GetType();
+                return goldRate;
+            }
 
 
 
-
-
-
-
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = client.GetAsync(urlUsd).Result;
+/*            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(urlCurrency).Result;
             response.EnsureSuccessStatusCode();
             string result = response.Content.ReadAsStringAsync().Result;
-            var currencyRate = JsonConvert.DeserializeObject<CurrencyRoot>(result);
+            var currencyRate = JsonConvert.DeserializeObject<CurrencyRoot>(result);*/
 
-            HttpClient clientGold = new HttpClient();
+
+            /*            HttpClient client = new HttpClient();
+                        HttpResponseMessage response = client.GetAsync(urlUsd).Result;
+                        response.EnsureSuccessStatusCode();
+                        string result = response.Content.ReadAsStringAsync().Result;
+                        var currencyRate = JsonConvert.DeserializeObject<CurrencyRoot>(result);*/
+
+
+
+
+/*            HttpClient clientGold = new HttpClient();
             HttpResponseMessage responseGold = clientGold.GetAsync(urlGold).Result;
             string resultGold = responseGold.Content.ReadAsStringAsync().Result;
-            var goldRate = JsonConvert.DeserializeObject<GoldRate[]>(resultGold);
-            /*            Console.WriteLine(currencyRate.rates[0].effectiveDate);
-                        Console.WriteLine(goldRate);
-                        Console.WriteLine(currencyRate)*/
-            ;
-            /*            Console.WriteLine(result);*/
-
-            /*            Exchange exchange = new Exchange();*/
-            List<Exchange> exchange = new List<Exchange>();
-
-            /*            object [,] exchangeRate = new object[30,2];
-                            for (int i = 0; i < 30; i++)
-                            {
-                                exchangeRate[i, 0] = currencyRate.rates[i].effectiveDate;
-                                exchangeRate[i, 1] = (goldRate[i].cena / currencyRate.rates[i].mid);
+            var goldRate = JsonConvert.DeserializeObject<GoldRate[]>(resultGold);*/
 
 
 
-                            }*/
+
+/*            List<Exchange> exchange = new List<Exchange>();
+
             double[] ep = new double[30];
             object[,] exchangeRate = new object[2, 30];
             for (int i = 0; i < 30; i++)
             {
-                var max = exchangeRate[1, 0];
                 exchangeRate[0, i] = currencyRate.rates[i].effectiveDate;
                 exchangeRate[1, i] = (goldRate[i].cena / currencyRate.rates[i].mid);
                 ep[i] = (goldRate[i].cena / currencyRate.rates[i].mid);
             }
-            /*            Console.WriteLine(exchange);
 
-                        Console.WriteLine(ep.ToList().IndexOf(ep.Max()));*/
-
-            /*            Console.WriteLine(ep.Max());*/
             double currently = Convert.ToDouble(exchangeRate[1, 29]);
-            Console.WriteLine("Maksymalny kurs złota wynosił: " + ep.Max() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Max())].ToString()));
-            Console.WriteLine("Minimalny kurs złota wynosił: " + ep.Min() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Min())].ToString()));
+            Console.WriteLine("Maksymalny kurs złota liczony w " + choosenCurrencyCode + " wynosił: " + ep.Max() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Max())].ToString()));
+            Console.WriteLine("Minimalny kurs złota liczony w " + choosenCurrencyCode + " wynosił: " + ep.Min() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Min())].ToString()));
             Console.WriteLine("Różnica między aktualnym a maksymalnym kursem wynosi :" + (currently - ep.Max()));
-            Console.WriteLine("Różnica między aktualnym a minimalnym kursem wynosi :" + (currently - ep.Min()));
+            Console.WriteLine("Różnica między aktualnym a minimalnym kursem wynosi :" + (currently - ep.Min()));*/
 
-            /*            Console.WriteLine(ep.Max());*/
-
-
-
-
-            /*          double[] ep = new double[30];
-                          for (int i = 0; i < 30; i++)
-                          {
-                              ep[i] = (goldRate[i].cena / currencyRate.rates[i].mid);
-
-
-
-
-                          }
-                          Console.WriteLine(ep.Max());
-
-
-                      Console.WriteLine(exchange);
-
-
-                          Console.WriteLine(exchangeRate);
-                          double min = 0, max = 0;
-                          for (int i = 0; i < 30; i++)
-                          {
+            var task = Task.Factory.ContinueWhenAll<object>(
+                /*                new Task[] { task1, task2 }, (values) =>*/
+                new Task[] { task1, task2 }, (values) =>
+                {
+                    List<object> resul = new List<object>();
+                    List<CurrencyRoot> ra = new List<CurrencyRoot>();
+                    foreach (Task<object> task in values)
+                    {
+                       
+                        resul.Add(task.Result);
+                        
+                    }
+                    Console.WriteLine((values[0] as Task<object>).Result);
+                   Console.WriteLine((values[1] as Task<object>).Result);
+                    
+                    double[] ep = new double[30];
+                    object[,] exchangeRate = new object[2, 30];
+                    
+                    var currencyRate = (values[0] as Task<object>).Result;
+                    var goldRate = (values[1] as Task<object>).Result;
 
 
+                    /*                    object currencyRate =  (values[0] as Task<object>).Result;*/
+                    System.Reflection.PropertyInfo pi = currencyRate.GetType().GetProperty("rates");
+                    exchangeRate = (object[,])(pi.GetValue(currencyRate, null));
+                    /*object currencyRate = task1.Result;*/
+                    /*                    var a = (values[0] as Task<object>).Result;
+                                        var bb = task1.Result;
 
-                          }
+                                        object goldRate = (values[1] as Task<object>).Result;*/
+                    /*                    var currencyRate = task1.Result;
+                                        var goldRate = task2.Result;*/
+                    Console.WriteLine(currencyRate);
+/*                    for (int i = 0; i < 30; i++)
+                    {
+                        exchangeRate[0, i] = currencyRate.rates[i].effectiveDate;
+                        exchangeRate[1, i] = (goldRate[i].cena / currencyRate.rates[i].mid);
+                        ep[i] = (goldRate[i].cena / currencyRate.rates[i].mid);
+                    }
 
+                    double currently = Convert.ToDouble(exchangeRate[1, 29]);
+                    Console.WriteLine("Maksymalny kurs złota liczony w " + choosenCurrencyCode + " wynosił: " + ep.Max() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Max())].ToString()));
+                    Console.WriteLine("Minimalny kurs złota liczony w " + choosenCurrencyCode + " wynosił: " + ep.Min() + " w dniu " + (exchangeRate[0, ep.ToList().IndexOf(ep.Min())].ToString()));
+                    Console.WriteLine("Różnica między aktualnym a maksymalnym kursem wynosi :" + (currently - ep.Max()));
+                    Console.WriteLine("Różnica między aktualnym a minimalnym kursem wynosi :" + (currently - ep.Min()));*/
+                    return "ok";
 
-
-                          foreach (var element in exchangeRate)
-                          {
-
-                              Console.WriteLine(element.GetType());
-                          }
-          */
-
-
+                }
+            );
+            Console.ReadLine();
         }
+     
     }
     
 }
---
+++ Tpl.cs
@@ -0,0 +1,76 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4_sa
+{
+    class Tpl
+    {
+           private string Work(object sender, CancellationToken token)
+            {
+                List<object> argslist = sender as List<object>;
+                string podsumowanie = null;
+
+                if (argslist != null)
+                {
+                    IFunction funkcja = (IFunction)argslist[0];
+
+                    for (int i = 1; i < 100; i++)
+                    {
+                        if (token.IsCancellationRequested)
+                        {
+                            Console.WriteLine("Przerwano zadanie.");
+                            podsumowanie = "";
+                            break;
+                        }
+
+                        powierzchnia += funkcja.GetY((decimal)rangeFrom + i * krok);
+                        Thread.Sleep(10);
+                        if (i % 10 == 0)
+                        {
+                            Console.WriteLine($"Postęp {i}%");
+                        }
+                    }
+
+                    powierzchnia = (powierzchnia + (funkcja.GetY(rangeFrom) + funkcja.GetY(rangeTo)) / 2) * krok;
+                    Console.WriteLine("Zadanie zakończone");
+                    podsumowanie += "Przybliżona wartość całki metodą trapezów dla przedziału: " + name + " wynosi " +
+                                    powierzchnia + Environment.NewLine;
+                    Console.WriteLine(podsumowanie);
+                }
+
+                return podsumowanie;
+            }
+
+            public string Name => "TPL";
+            public int Id => 2;
+
+            public void Run(IFunction function, decimal rangeFrom, decimal rangeTo, string name)
+            {
+                List<object> arguments1 = new()
+                {
+                    function,
+                    rangeFrom,
+                    rangeTo,
+                    name
+                };
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+                CancellationToken token = tokenSource.Token;
+
+                Task<string> task1 = new Task<string>(
+                    () => Work(arguments1, token), token);
+                task1.Start();
+
+                if (Console.ReadKey(true).KeyChar == 'c')
+                {
+                    tokenSource.Cancel();
+                }
+
+                Console.WriteLine("Koniec zadania");
+            }
+        
+    }
+}
+*/
